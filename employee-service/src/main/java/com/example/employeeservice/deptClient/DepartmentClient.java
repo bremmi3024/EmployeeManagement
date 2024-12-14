5ee3e31de398e8af9b4b9d9f3e8323b839c6985a
bremmi3024 <bremmi3024@gmail.com>
2024-12-14 15:34:27 +0530

    removed the url for the service and doing with service discovery load balencing

--- employee-service/src/main/java/com/example/employeeservice/deptClient/DepartmentClient.java
+++ employee-service/src/main/java/com/example/employeeservice/deptClient/DepartmentClient.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="department-service",url="http://localhost:8082")
+//@FeignClient(name="department-service",url="http://localhost:8082")
+@FeignClient(name="department-service")
 public interface DepartmentClient {
 
 	@GetMapping("/departments/{id}")
